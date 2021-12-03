77e012651f7802e4deb8bd2167d3084020c56dad
jodhernandezbe <jodhernandezbemj@gmail.com>
2021-12-03 13:37:45 +0100

    Version Dec 03, 2021

--- static/js/functions.js
+++ static/js/functions.js
@@ -19,19 +19,19 @@ function openFullscreen(id) {
 
 $(window).resize(function() {
     if ( document.URL.includes("") ){
-        var height = $(window).height() - ($("#header").outerHeight(true) + $("#footer").outerHeight(true));
+        var height = $(window).height() - ($("#header").outerHeight() + $("#footer").outerHeight());
         $("#main").css("min-height",height+"px");
-        // $(".carousel-inner img").css("min-height",height+"px");
+        $(".carousel-inner img").css("height",height+"px");
     }
 });
 
 
-$(function(){
-    $(document).ready(function(){
-        var height = $(window).height();
-var header =$('header.site-navbar').height();
-var finalheight =height-header;
-// alert(finalwidth);
-$('.carousel-item img').css('height',''+finalheight+'px');
-});
-    });
+// $(function(){
+//     $(document).ready(function(){
+//         var height = $(window).height();
+// var header =$('header.site-navbar').height();
+// var finalheight =height-header;
+// // alert(finalwidth);
+// $('.carousel-item img').css('height',''+finalheight+'px');
+// });
+//     });
