e07a235b1d6271ced063eec027228d5e8fc66c9a
jodhernandezbe <jodhernandezbemj@gmail.com>
2021-12-03 16:38:11 +0100

    Version Dec 03, 2021

--- static/js/functions.js
+++ static/js/functions.js
@@ -17,21 +17,17 @@ function openFullscreen(id) {
 }
 
 
-$(window).resize(function() {
-    if ( document.URL.includes("") ){
+if ( window.location.pathname == '/' ){
+    if ($(window).width() >= 768){
         var height = $(window).height() - ($("#header").outerHeight() + $("#footer").outerHeight());
-        $("#main").css("min-height",height+"px");
+        $("#main").css("height",height+"px");
         $(".carousel-inner img").css("height",height+"px");
+        $(".carousel-inner").css("height",height+"px");
+        $(".carousel").css("height",height+"px");
+        var width = $(window).width();
+        $("#main").css("width",width+"px");
+        $(".carousel-inner img").css("width",width+"px");
+        $(".carousel-inner").css("width",width+"px");
+        $(".carousel").css("width",width+"px");
     }
-});
-
-
-// $(function(){
-//     $(document).ready(function(){
-//         var height = $(window).height();
-// var header =$('header.site-navbar').height();
-// var finalheight =height-header;
-// // alert(finalwidth);
-// $('.carousel-item img').css('height',''+finalheight+'px');
-// });
-//     });
+};
